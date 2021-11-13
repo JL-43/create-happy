b43b022ee7334791a5957ff2f4e19eeb2c3e74ad
JL <joseluisreyes43@gmail.com>
2021-11-13 15:30:43 +0800

    changed the way the color styles work for the cards

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 const Card = (props) => {
-	const highlight = `h-1 bg-${props.color}-300 flex w-full`;
+	const parentClass = `rounded overflow-hidden shadow-lg flex flex-col h-72 mb-10 text-left border-t-4 border-1 border-${props.color}-400`;
 	return (
-		<div className='rounded overflow-hidden shadow-lg flex flex-col h-72 mb-10 text-left border-b-2 border-r-2 border-l-2'>
-			<div className={highlight}></div>
+		<div className={parentClass}>
 			<div className='flex justify-around'>
 				<div className='flex-auto px-4 py-4'>
 					<p>{props.title}</p>
